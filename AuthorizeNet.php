85d1060e7084c981685b70bf1ce529335f6dfa48
Baptiste Cabarrou <bcabarrou@openstudio.fr>
2015-10-23 16:56:09 +0200

    Add module default config values

--- AuthorizeNet.php
+++ AuthorizeNet.php
@@ -2,12 +2,18 @@
 
 namespace AuthorizeNet;
 
+use AuthorizeNet\Config\ConfigKeys;
 use AuthorizeNet\Service\SIM\RequestServiceInterface;
 use Thelia\Model\Order;
 use Thelia\Module\AbstractPaymentModule;
 
 class AuthorizeNet extends AbstractPaymentModule
 {
+    protected static $defaultConfigValues = [
+        ConfigKeys::GATEWAY_URL => 'https://secure2.authorize.net/gateway/transact.dll',
+        ConfigKeys::TRANSACTION_VERSION => '3.1',
+    ];
+
     public function pay(Order $order)
     {
         /** @var RequestServiceInterface $SIMRequestService */
@@ -23,5 +29,14 @@ class AuthorizeNet extends AbstractPaymentModule
     public function isValidPayment()
     {
         return true;
+    }
+
+    public static function getConfigValue($variableName, $defaultValue = null, $valueLocale = null)
+    {
+        if ($defaultValue === null && isset(static::$defaultConfigValues[$variableName])) {
+            $defaultValue = static::$defaultConfigValues[$variableName];
+        }
+
+        return parent::getConfigValue($variableName, $defaultValue, $valueLocale);
     }
 }
